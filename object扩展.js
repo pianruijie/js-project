a5a9316b42a72ed62f0fbabeb50397989981ee0c
骈瑞杰 <jerry.pian@yingzt.com>
2019-10-18 11:25:13 +0800

    冒泡捕获

---
+++ object扩展.js
@@ -0,0 +1,10 @@
+/**
+ * @功能名称:
+ * @文件名称: object扩展.js
+ * @Date: 2019/9/11 下午5:18.
+ * @Author: pianruijie
+ * @Copyright（C）: 2019 X-Financial Inc.   All rights reserved.
+ * 注意：本内容仅限于小赢科技有限责任公司内部传阅，禁止外泄以及用于其他的商业目的。
+ */
+//对象的扩展运算符
+let a = {a:1,b:2,c:3}
